c78970e95e2fcb011117486a2490bba26659fe70
Amin <M.amin.ansari@gmail.com>
2014-04-25 18:51:16 +0430

    Initial Commit

---
+++ ip Planner/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ip Planner
+//
+//  Created by Mohammad Amin Ansari on 4/25/14.
+//  Copyright (c) 2014 Mohammad Amin Ansari. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
